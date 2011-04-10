c7594aacf979f1f9a19891c099e20a7584237cd1
shingrus <shingrus@gmail.com>
2011-04-10 17:51:20 +0400

    add new files

---
+++ src/SwitcherApplication.java
@@ -0,0 +1,6 @@
+import android.app.Application;
+
+
+public class SwitcherApplication extends Application {
+
+}
---
+++ src/SwitcherService.java
@@ -0,0 +1,14 @@
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class SwitcherService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
