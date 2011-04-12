2f7bbe1811738124be951ae201bfe52d61020842
shingrus <shingrus@gmail.com>
2011-04-12 18:13:45 +0400

    added Foreground service and test notifier

--- gen/com/shingrus/wswitcher/R.java
+++ gen/com/shingrus/wswitcher/R.java
@@ -19,5 +19,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int notification_message=0x7f040002;
+        public static final int notification_title=0x7f040003;
+        public static final int ticker_text=0x7f040004;
     }
 }
--- src/com/shingrus/wswitcher/SwitcherService.java
+++ src/com/shingrus/wswitcher/SwitcherService.java
@@ -1,6 +1,8 @@
 package com.shingrus.wswitcher;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -20,6 +22,13 @@ public class SwitcherService extends Service {
 
 	@Override
 	public void onStart(Intent intent, int startId) {
+		Notification notification = new Notification(R.drawable.icon, getText(R.string.ticker_text),
+		        System.currentTimeMillis());
+		Intent notificationIntent = new Intent(this, SwitcherSettingsActivity.class);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(this, getText(R.string.notification_title),
+		        getText(R.string.notification_message), pendingIntent);
+		startForeground(100500, notification);
 		super.onStart(intent, startId);
 	}
 }
