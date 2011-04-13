26b5fb9ba940f40442f38c655a04638299292764
shingrus <shingrus@gmail.com>
2011-04-13 09:12:25 +0400

    show notification

--- src/com/shingrus/wswitcher/SwitcherService.java
+++ src/com/shingrus/wswitcher/SwitcherService.java
@@ -9,9 +9,22 @@ import android.os.IBinder;
 
 public class SwitcherService extends Service {
 
+	Notification notification;
+	
+	public SwitcherService() {
+		super();
+		notification = new Notification(R.drawable.icon, getText(R.string.ticker_text),
+		        System.currentTimeMillis());
+		Intent notificationIntent = new Intent(this, SwitcherSettingsActivity.class);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(this, getText(R.string.notification_title),
+		        getText(R.string.notification_message), pendingIntent);
+	}
+
 	@Override
 	public void onCreate() {
 		System.out.println("Create new service");
+		
 		super.onCreate();
 	}
 
@@ -22,12 +35,7 @@ public class SwitcherService extends Service {
 
 	@Override
 	public void onStart(Intent intent, int startId) {
-		Notification notification = new Notification(R.drawable.icon, getText(R.string.ticker_text),
-		        System.currentTimeMillis());
-		Intent notificationIntent = new Intent(this, SwitcherSettingsActivity.class);
-		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-		notification.setLatestEventInfo(this, getText(R.string.notification_title),
-		        getText(R.string.notification_message), pendingIntent);
+		
 		startForeground(100500, notification);
 		super.onStart(intent, startId);
 	}
