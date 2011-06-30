5f9cf999bc2664c3e41b6a7d1b104436d09cd72e
shingrus <shingrus@gmail.com>
2011-06-30 16:52:00 +0400

    table layout with right button position

--- gen/com/shingrus/wswitcher/R.java
+++ gen/com/shingrus/wswitcher/R.java
@@ -15,9 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f060001;
-        public static final int textNotification=0x7f060002;
         public static final int textView1=0x7f060000;
-        public static final int wifiButton=0x7f060003;
     }
     public static final class integer {
         public static final int notyfyId=0x7f050000;
